95b19f29c15d06b4bd1c3c7d82624e3da6e5bea3
Nicholas Whitehead <nwhitehead@drivewealth.it>
2019-04-16 11:37:52 -0400

    Added oh-cache. Enabled all tests.

--- src/main/java/com/heliosapm/benchmark/maps/MapBenchmark.java
+++ src/main/java/com/heliosapm/benchmark/maps/MapBenchmark.java
@@ -2,6 +2,7 @@
 package com.heliosapm.benchmark.maps;
 
 import java.lang.management.ManagementFactory;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.LongSummaryStatistics;
 import java.util.UUID;
@@ -20,8 +21,12 @@ import java.util.stream.LongStream;
 import javax.management.JMX;
 import javax.management.ObjectName;
 
+import org.caffinitas.ohc.CacheSerializer;
+import org.caffinitas.ohc.OHCache;
+import org.caffinitas.ohc.OHCacheBuilder;
 import org.jctools.maps.NonBlockingHashMapLong;
 
+import com.google.common.base.Charsets;
 import com.sun.management.ThreadMXBean;
 
 /**
@@ -31,14 +36,14 @@ import com.sun.management.ThreadMXBean;
  * <p><code>com.heliosapm.benchmark.maps.MapBenchmark</code></p>
  */
 public class MapBenchmark {
-	public static final boolean MUTATE = false;
+	public static final boolean MUTATE = true;
 	public static final int WARMUP_LOOPS = 2;
 	public static final int LOOKUP_LOOPS = 10_000;
 	public static final int MAP_SIZE = 100_000;
 	public static final String[] MAP_VALUES;
 	public static final String[] ALT_MAP_VALUES;
 	public static final long[] MAP_KEYS;
-	public static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors();
+	public static final int THREAD_COUNT = Runtime.getRuntime().availableProcessors()  * 2;
 	public static final ThreadMXBean threadMxBean = threadMxBean();
 	
 	
@@ -116,64 +121,43 @@ public class MapBenchmark {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		log("Initializing CHM Benchmark");
-		final ConcurrentHashMap<Long, String> chm = new ConcurrentHashMap<>(MAP_SIZE);
-		IntStream.range(0, MAP_SIZE).parallel().forEach(i -> {
-			chm.put(MAP_KEYS[i], MAP_VALUES[i]);
-		});
-		log("CHM Size: " + MemoryMeasurer.measureBytes(chm));
-		log("Starting CHM Benchmark Warmup");
+		concurrentHashMapTest();
+		nonBlockingHashMapLongTest();
+		guavaCacheTest();				
+		caffeineTest();
+		cache2kTest();		
+		oHCacheTest();
+	}
+	
+	public static void cache2kTest() {
+		log("Initializing Cache2k Benchmark");
+		final org.cache2k.Cache<Long, String> cache2k = org.cache2k.Cache2kBuilder.of(Long.class, String.class)
+				.entryCapacity(MAP_SIZE)
+				.build();
+		time("Cache2k Load", () -> IntStream.range(0, MAP_SIZE).parallel().forEach(i -> {
+			cache2k.put(MAP_KEYS[i], MAP_VALUES[i]);
+		}));
+		log("Cache2k Memory Size: " + MemoryMeasurer.measureBytes(cache2k));
+		log("Starting Cache2k Benchmark Warmup");
 		for(int i = 0; i < WARMUP_LOOPS; i++) {
-			bench(chm, LOOKUP_LOOPS, false, MUTATE);
+			bench(cache2k, LOOKUP_LOOPS, false, MUTATE);
 		}
 		System.gc();
-		log("Starting CHM Benchmark");
-		bench(chm, LOOKUP_LOOPS, true, MUTATE);
-		chm.clear();
+		log("Starting Cache2k Benchmark");
+		bench(cache2k, LOOKUP_LOOPS, true, MUTATE);
+		cache2k.clearAndClose();
 		System.gc();
-		log("Initializing NBHML Benchmark");
-		final NonBlockingHashMapLong<String> nbhml = new NonBlockingHashMapLong<>(MAP_SIZE, true);
-		IntStream.range(0, MAP_SIZE).parallel().forEach(i -> {
-			nbhml.put(MAP_KEYS[i], MAP_VALUES[i]);
-		});		
-		log("NBHML Size: " + MemoryMeasurer.measureBytes(nbhml));
-		log("Starting NBHML Benchmark Warmup");
-		for(int i = 0; i < WARMUP_LOOPS; i++) {
-			bench(nbhml, LOOKUP_LOOPS, false, MUTATE);
-		}
-		System.gc();
-		log("Starting NBHML Benchmark");
-		bench(nbhml, LOOKUP_LOOPS, true, MUTATE);
-		nbhml.clear();
-		
-		System.gc();
-		log("Initializing GuavaCache Benchmark");
-		final com.google.common.cache.Cache<Long, String> guava = com.google.common.cache.CacheBuilder.newBuilder()
-			.initialCapacity(MAP_SIZE)
-			.build();
-		IntStream.range(0, MAP_SIZE).parallel().forEach(i -> {
-			guava.put(MAP_KEYS[i], MAP_VALUES[i]);
-		});		
-		log("GuavaCache Size: " + MemoryMeasurer.measureBytes(guava));
-		log("Starting GuavaCache Benchmark Warmup");
-		for(int i = 0; i < WARMUP_LOOPS; i++) {
-			bench(guava, LOOKUP_LOOPS, false, MUTATE);
-		}
-		System.gc();
-		log("Starting GuavaCache Benchmark");
-		bench(guava, LOOKUP_LOOPS, true, MUTATE);
-		guava.invalidateAll();
-		guava.cleanUp();
-		
-		System.gc();
+	}
+	
+	public static void caffeineTest() {
 		log("Initializing CaffeineCache Benchmark");
 		final com.github.benmanes.caffeine.cache.Cache<Long, String> caffeine = com.github.benmanes.caffeine.cache.Caffeine.newBuilder()
 				.initialCapacity(MAP_SIZE)
 				.build();
-		IntStream.range(0, MAP_SIZE).parallel().forEach(i -> {
+		time("Caffeine Load", () -> IntStream.range(0, MAP_SIZE).parallel().forEach(i -> {
 			caffeine.put(MAP_KEYS[i], MAP_VALUES[i]);
-		});		
-		log("CaffeineCache Size: " + MemoryMeasurer.measureBytes(caffeine));
+		}));
+		log("CaffeineCache Memory Size: " + MemoryMeasurer.measureBytes(caffeine));
 		log("Starting CaffeineCache Benchmark Warmup");
 		for(int i = 0; i < WARMUP_LOOPS; i++) {
 			bench(caffeine, LOOKUP_LOOPS, false, MUTATE);
@@ -183,8 +167,130 @@ public class MapBenchmark {
 		bench(caffeine, LOOKUP_LOOPS, true, MUTATE);
 		caffeine.invalidateAll();
 		caffeine.cleanUp();
-
+		System.gc();		
 	}
+	
+	public static void guavaCacheTest() {
+		log("Initializing GuavaCache Benchmark");
+		final com.google.common.cache.Cache<Long, String> guava = com.google.common.cache.CacheBuilder.newBuilder()
+			.initialCapacity(MAP_SIZE)
+			.build();
+		time("Guava Load", () -> IntStream.range(0, MAP_SIZE).parallel().forEach(i -> {
+			guava.put(MAP_KEYS[i], MAP_VALUES[i]);
+		}));
+		log("GuavaCache Memory Size: " + MemoryMeasurer.measureBytes(guava));
+		log("Starting GuavaCache Benchmark Warmup");
+		for(int i = 0; i < WARMUP_LOOPS; i++) {
+			bench(guava, LOOKUP_LOOPS, false, MUTATE);
+		}
+		System.gc();
+		log("Starting GuavaCache Benchmark");
+		bench(guava, LOOKUP_LOOPS, true, MUTATE);
+		guava.invalidateAll();
+		guava.cleanUp();
+		System.gc();
+	}
+	
+	public static void nonBlockingHashMapLongTest() {
+		log("Initializing NBHML Benchmark");
+		final NonBlockingHashMapLong<String> nbhml = new NonBlockingHashMapLong<>(MAP_SIZE, true);
+		time("Load NBHML", () -> IntStream.range(0, MAP_SIZE).parallel().forEach(i -> {
+			nbhml.put(MAP_KEYS[i], MAP_VALUES[i]);
+		}));
+		log("NBHML Memory Size: " + MemoryMeasurer.measureBytes(nbhml));
+		log("Starting NBHML Benchmark Warmup");
+		for(int i = 0; i < WARMUP_LOOPS; i++) {
+			bench(nbhml, LOOKUP_LOOPS, false, MUTATE);
+		}
+		System.gc();
+		log("Starting NBHML Benchmark");
+		bench(nbhml, LOOKUP_LOOPS, true, MUTATE);
+		nbhml.clear();
+		System.gc();
+	}
+	
+	public static void concurrentHashMapTest() {
+		log("Initializing CHM Benchmark");
+		final ConcurrentHashMap<Long, String> chm = new ConcurrentHashMap<>(MAP_SIZE);
+		time("Load CHM", () ->
+			IntStream.range(0, MAP_SIZE).parallel().forEach(i -> {
+				chm.put(MAP_KEYS[i], MAP_VALUES[i]);
+			})
+		);
+		log("CHM Memory Size: " + MemoryMeasurer.measureBytes(chm));
+		log("Starting CHM Benchmark Warmup");
+		for(int i = 0; i < WARMUP_LOOPS; i++) {
+			bench(chm, LOOKUP_LOOPS, false, MUTATE);
+		}
+		System.gc();
+		log("Starting CHM Benchmark");
+		bench(chm, LOOKUP_LOOPS, true, MUTATE);
+		chm.clear();
+		System.gc();
+	}
+	
+	public static void oHCacheTest() {
+		final CacheSerializer<Long> longSerializer = new CacheSerializer<Long>() {
+
+			@Override
+			public void serialize(Long value, ByteBuffer buf) {
+				buf.putLong(value.longValue());
+				
+			}
+
+			@Override
+			public Long deserialize(ByteBuffer buf) {
+				return buf.getLong();
+			}
+
+			@Override
+			public int serializedSize(Long value) {
+				return 8;
+			}
+			
+		};
+		
+		final CacheSerializer<String> stringSerializer = new CacheSerializer<String>() {
+	        public void serialize(String s, ByteBuffer buf) {
+	            byte[] bytes = s.getBytes(Charsets.UTF_8);
+	            buf.put((byte) ((bytes.length >>> 8) & 0xFF));
+	            buf.put((byte) ((bytes.length >>> 0) & 0xFF));
+	            buf.put(bytes);
+	        }
+
+	        public String deserialize(ByteBuffer buf) {
+	            int length = (((buf.get() & 0xff) << 8) + ((buf.get() & 0xff) << 0));
+	            byte[] bytes = new byte[length];
+	            buf.get(bytes);
+	            return new String(bytes, Charsets.UTF_8);
+	        }
+
+	        public int serializedSize(String s) {
+	            return writeUTFLen(s);
+	        }
+	    };		
+		
+		log("Initializing OHCache Benchmark");
+		final OHCache<Long, String> ohcache = OHCacheBuilder.<Long, String>newBuilder()				
+				.capacity(MAP_SIZE)
+				.valueSerializer(stringSerializer)
+				.keySerializer(longSerializer)
+				.build();
+		time("Load OHCache", () -> IntStream.range(0, MAP_SIZE).parallel().forEach(i -> {
+			ohcache.put(MAP_KEYS[i], MAP_VALUES[i]);
+		}));
+		log("OHCache Memory Size: " + MemoryMeasurer.measureBytes(ohcache));
+		log("Starting OHCache Benchmark Warmup");
+		for(int i = 0; i < WARMUP_LOOPS; i++) {
+			bench(ohcache, LOOKUP_LOOPS, false, MUTATE);
+		}
+		System.gc();
+		log("Starting OHCache Benchmark");
+		bench(ohcache, LOOKUP_LOOPS, true, MUTATE);
+		ohcache.clear();
+		System.gc();
+	}
+	
 	
 	
 	public static void bench(final NonBlockingHashMapLong<String> map, final int loops, final boolean logThis, final boolean mutate) {		
@@ -402,5 +508,135 @@ public class MapBenchmark {
 		if(logThis) log("CaffeineCache CPU Time (sec):" + totalCpu);
 		if(logThis) log("CaffeineCache JitSum:" + jitSum);
 	}
+	
+	public static void bench(final org.cache2k.Cache<Long, String> map, final int loops, final boolean logThis, final boolean mutate) {		
+		final long[] elapsedTimes = new long[THREAD_COUNT];
+		final long[] allocatedMem = new long[THREAD_COUNT];
+		final long[] cpuTime = new long[THREAD_COUNT];
+		final long[] jitAvoiders = new long[THREAD_COUNT];
+		final CountDownLatch startLatch = new CountDownLatch(1);
+		final CountDownLatch completeLatch = new CountDownLatch(THREAD_COUNT);
+		for(int i = 0; i < THREAD_COUNT; i++) {
+			final int threadSeq = i;
+			THREAD_POOL.execute(() -> {
+				try {
+					final long threadId = Thread.currentThread().getId();
+					try {
+						startLatch.await();
+					} catch (Exception x) {}
+					final long startCpu = getThreadCPUTime(threadId);
+					final long startMem = getThreadAllocatedMem(threadId);
+					final long startTime = System.currentTimeMillis();
+					int jitAvoider = 0;
+					for(int x = 0; x < loops; x++) {
+						for(int y = 0; y < MAP_SIZE; y++) {
+							if (map.get(MAP_KEYS[y]) != null) {
+								jitAvoider++;
+							}
+							if (mutate) {
+								map.put(MAP_KEYS[y], y%2==0 ? MAP_VALUES[y] : ALT_MAP_VALUES[y]); 									
+							}							
+						}
+					}					
+					elapsedTimes[threadSeq] = System.currentTimeMillis() - startTime;
+					allocatedMem[threadSeq] = getThreadAllocatedMem(threadId) - startMem;
+					cpuTime[threadSeq] = getThreadCPUTime(threadId) - startCpu;
+					jitAvoiders[threadSeq] = jitAvoider;
+				} finally {
+					completeLatch.countDown();
+				}
+			});
+		}
+		startLatch.countDown();
+		if(logThis) log("Cache2k Waiting for completion...");
+		try {
+			completeLatch.await();
+		} catch (Exception x) {}
+		if(logThis) log("Cache2k Complete");
+		LongSummaryStatistics timeStats = Arrays.stream(elapsedTimes).summaryStatistics();
+		LongSummaryStatistics memStats = Arrays.stream(allocatedMem).summaryStatistics();
+		long totalCpu = TimeUnit.NANOSECONDS.toSeconds(Arrays.stream(cpuTime).sum());
+		long jitSum = Arrays.stream(jitAvoiders).sum();
+		if(logThis) log("Cache2k Times:" + timeStats);
+		if(logThis) log("Cache2k MemAlloc:" + memStats);
+		if(logThis) log("Cache2k CPU Time (sec):" + totalCpu);
+		if(logThis) log("Cache2k JitSum:" + jitSum);
+	}
+	
+	public static void bench(final OHCache<Long, String> map, final int loops, final boolean logThis, final boolean mutate) {		
+		final long[] elapsedTimes = new long[THREAD_COUNT];
+		final long[] allocatedMem = new long[THREAD_COUNT];
+		final long[] cpuTime = new long[THREAD_COUNT];
+		final long[] jitAvoiders = new long[THREAD_COUNT];
+		final CountDownLatch startLatch = new CountDownLatch(1);
+		final CountDownLatch completeLatch = new CountDownLatch(THREAD_COUNT);
+		for(int i = 0; i < THREAD_COUNT; i++) {
+			final int threadSeq = i;
+			THREAD_POOL.execute(() -> {
+				try {
+					final long threadId = Thread.currentThread().getId();
+					try {
+						startLatch.await();
+					} catch (Exception x) {}
+					final long startCpu = getThreadCPUTime(threadId);
+					final long startMem = getThreadAllocatedMem(threadId);
+					final long startTime = System.currentTimeMillis();
+					int jitAvoider = 0;
+					for(int x = 0; x < loops; x++) {
+						for(int y = 0; y < MAP_SIZE; y++) {
+							if (map.get(MAP_KEYS[y]) != null) {
+								jitAvoider++;
+							}
+							if (mutate) {
+								map.put(MAP_KEYS[y], y%2==0 ? MAP_VALUES[y] : ALT_MAP_VALUES[y]); 									
+							}							
+						}
+					}					
+					elapsedTimes[threadSeq] = System.currentTimeMillis() - startTime;
+					allocatedMem[threadSeq] = getThreadAllocatedMem(threadId) - startMem;
+					cpuTime[threadSeq] = getThreadCPUTime(threadId) - startCpu;
+					jitAvoiders[threadSeq] = jitAvoider;
+				} finally {
+					completeLatch.countDown();
+				}
+			});
+		}
+		startLatch.countDown();
+		if(logThis) log("OHCache Waiting for completion...");
+		try {
+			completeLatch.await();
+		} catch (Exception x) {}
+		if(logThis) log("OHCache Complete");
+		LongSummaryStatistics timeStats = Arrays.stream(elapsedTimes).summaryStatistics();
+		LongSummaryStatistics memStats = Arrays.stream(allocatedMem).summaryStatistics();
+		long totalCpu = TimeUnit.NANOSECONDS.toSeconds(Arrays.stream(cpuTime).sum());
+		long jitSum = Arrays.stream(jitAvoiders).sum();
+		if(logThis) log("OHCache Times:" + timeStats);
+		if(logThis) log("OHCache MemAlloc:" + memStats);
+		if(logThis) log("OHCache CPU Time (sec):" + totalCpu);
+		if(logThis) log("OHCache JitSum:" + jitSum);
+	}
+	
+	 static int writeUTFLen(String str) {
+	        int strlen = str.length();
+	        int utflen = 0;
+	        int c;
+
+	        for (int i = 0; i < strlen; i++) {
+	            c = str.charAt(i);
+	            if ((c >= 0x0001) && (c <= 0x007F))
+	                utflen++;
+	            else if (c > 0x07FF)
+	                utflen += 3;
+	            else
+	                utflen += 2;
+	        }
+
+	        if (utflen > 65535)
+	            throw new RuntimeException("encoded string too long: " + utflen + " bytes");
+
+	        return utflen + 2;
+	    }
+	
 
 }
